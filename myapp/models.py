bef5d3603a0316a5425c9d3a2249d196f8ee7831
pfbrodon <pfbrodon@gmail.com>
2023-12-18 00:25:12 -0300

    mac

--- myapp/models.py
+++ myapp/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Proyecto(models.Model):
+    nombre=models.CharField(max_length=200)
+    
+class Tareas(models.Model):
+    titulo=models.CharField(max_length=100)
+    descripcion=models.TextField()
+    proyecto=models.ForeignKey(Proyecto, on_delete=models.CASCADE)
--- myapp/views.py
+++ myapp/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse 
 
 # Create your views here.
 def hello(request):
     return HttpResponse("Hello World")
+
+def about(request):
+    return HttpResponse("About")
+
